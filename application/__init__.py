378928b115f9c3ee8ea29cec9f125925b56a2100
Irene211 <42807863+Irene211@users.noreply.github.com>
2020-05-05 01:52:14 -0400

    Try our model again

--- application/__init__.py
+++ application/__init__.py
@@ -1,42 +1,31 @@
 from flask import Flask, request, Response, json
 import numpy as np
-from sklearn import datasets
-from sklearn.datasets import load_iris 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import pickle
+import pandas as pd
+import math
+import re
+from scipy.sparse import csr_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+#from surprise import Reader, Dataset, SVD
+sns.set_style("darkgrid")
+#from surprise.model_selection import cross_validate, KFold
+from model import Pearson_Model
 
 app = Flask(__name__)
 
-# load data
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
-
-# train model
-X_train, X_test, y_train, y_test = train_test_split(X, y)
-rfc = RandomForestClassifier(n_estimators = 100, n_jobs = 2)
-rfc.fit(X_train, y_train)
-
-#create pickle file
-filename = 'rfc_model'
-pickle.dump(rfc, open(filename, 'wb'))
-
-# Load the model from pickle
-rfc_model = pickle.load(open(filename,'rb'))
+loaded_p_model = pickle.load(open("C:/Users/Yelena/Desktop/p_model.sav", 'rb'))
+loaded_p_model.recommend("The Lion King: Special Edition", 10)
 
 #create api
 @app.route('/api/', methods=['GET', 'POST'])
 @app.route('/api',methods=['GET', 'POST'])
 def predict():
-    # Get the data from POST request
     data = request.get_json(force=True)
-    requestData = [data["sepallength"], data["sepalwidth"], data["petallength"], data["petalwidth"]]
-    requestData = np.array([requestData])
 
-    # Make prediction using model 
-    prediction = rfc_model.predict(requestData)
-    return Response(json.dumps(int(prediction[0])))
+    return Response(loaded_p_model.recommend(data, 10))
 
 if __name__ == '__main__':
    app.run()
